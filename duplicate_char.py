1d87fea332b4f3f23d248f196eb7ab7c0ec55411
ray-ray <rayturla21@yahoo.com>
2022-08-19 14:03:48 +0800

    Add duplicate_char

---
+++ duplicate_char.py
@@ -0,0 +1,59 @@
+import math
+from utils.timeit import timeit
+
+"""
+This file consists of different implementations of a function that accepts an input of string and returns true if an input has a duplicate character.
+"""
+
+@timeit
+def dictDup(key):
+  strlen = len(key)
+  median = int(math.floor(strlen / 2))
+  dupDict = {}
+  left = [key[s] for s in range(0, median)]
+  right = [key[s] for s in range(median, strlen)]
+  for i in range(0, len(right)):
+    if not left[i] in dupDict:
+      dupDict[left[i]] = True
+    else:
+      return True
+    if not right[i] in dupDict:
+      dupDict[right[i]] = True
+    else:
+      return True
+  print('f')
+  return False
+
+@timeit
+def isDuplicate(key):
+  strlen = len(key)
+  median = int(math.floor(strlen / 2))
+  left = [key[s] for s in range(0, median)]
+  right = [key[s] for s in range(median, strlen)]
+  for i in range(0, len(right)):
+    if i < len(left):
+      for j in range(len(right)):
+        jshift = j + 1
+        if jshift < len(left) and not i == jshift:
+          if(left[i] == left[jshift]):
+            return True
+          if(right[i] == right[jshift]):
+            return True
+        if(left[i] == right[j]):
+          return True
+  return False
+
+@timeit
+def selectD(key):
+  strlen = len(key)
+  for i in range(0, strlen):
+    for j in range(0, strlen):
+      jshift = j + 1
+      if jshift < strlen and not i == jshift:
+        if(key[i] == key[jshift]):
+          return True
+  return False
+
+selectD("1234567890-qwertyuiop[asdfghjklzxcvbnm,!@#$%^&*()BbPOIUYTRE")
+isDuplicate("1234567890-qwertyuiop[asdfghjklzxcvbnm,!@#$%^&*()BbPOIUYTRE")
+dictDup("1234567890-qwertyuiop[asdfghjklzxcvbnm,!@#$%^&*()BbPOIUYTRE")
